3200a4104f4c7eaf5b5dacdc803b849bc2883d8d
tkyg <80114433+tkyg@users.noreply.github.com>
2022-08-17 17:28:43 -0400

    commit tree 5834bc648fbde59f57340a49aa7182c7f155a75c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+greet "Sunny"
+
+def add (num1, num2)
+  return num1 + num2
+end
+sum = add(1, 2)
+
+def halve(number)
+  if number.class != Integer
+    return nil
+  end
+  number / 2
+end
